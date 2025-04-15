c1dd59a4894466009e0f0e7bc37d62a18a8e9c15
anotherfaizan <mdfaizankhan2002@gmail.com>
2025-04-15 12:05:53 +0530

    Adds Mapper class
    Conversion between DAO Entity and DTO

---
+++ src/main/java/com/fz/myBank/mappers/TransactionMapper.java
@@ -0,0 +1,31 @@
+package com.fz.myBank.mappers;
+
+import com.fz.myBank.dao.Transaction;
+import com.fz.myBank.dto.TransactionDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionMapper {
+    public static Transaction mapToTransaction(TransactionDto transactionDto){
+        Transaction transaction = new Transaction();
+        BeanUtils.copyProperties(transactionDto, transaction);
+        return  transaction;
+    }
+
+    public static TransactionDto mapToTransactionDto(Transaction transaction){
+        TransactionDto transactionDto = new TransactionDto();
+        BeanUtils.copyProperties(transaction, transactionDto);
+        return transactionDto;
+    }
+
+    public static List<TransactionDto> mapToTransactonDtoList(List<Transaction> transactions){
+        List<TransactionDto> transactionDtoList = new ArrayList<>();
+        for(Transaction transaction : transactions){
+            transactionDtoList.add(mapToTransactionDto(transaction));
+        }
+        return transactionDtoList;
+    }
+}
